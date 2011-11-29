fc42508eb93cabeae7fed091b4ee65200da401e9
Josh Suereth <joshua.suereth@gmail.com>
2011-11-28 21:33:35 -0500

    Added man1 doc and gzipped documentation to package generation

--- project/build.scala
+++ project/build.scala
@@ -8,13 +8,33 @@ object SbtExtras extends Build {
 object DebianPkg {
   val Debian = config("debian-pkg")
   val makeDebianExplodedPackage = TaskKey[File]("make-debian-exploded-package")
+  val makeZippedPackageSource = TaskKey[Unit]("make-zipped-package-source")
+  val genControlFile = TaskKey[File]("generate-control-file")
 
   val settings: Seq[Setting[_]] = Seq(
     resourceDirectory in Debian <<= baseDirectory(_ / "src" / "debian"),
-    target in Debian <<= target(_ / "debian"),
+    resourceDirectory in Debian in makeZippedPackageSource <<= baseDirectory(_ / "src" / "debian-gzipped"),
     mappings in Debian <<= resourceDirectory in Debian map { d => (d.*** --- d) x (relativeTo(d)) },
+    mappings in Debian in makeZippedPackageSource <<= resourceDirectory in Debian in makeZippedPackageSource map { d => (d.*** --- d) x (relativeTo(d)) },
+    // TODO - put sbt-version into the generated sbt script.
     mappings in Debian <+= baseDirectory map { dir => (dir / "sbt" -> "usr/bin/sbt") },
-    makeDebianExplodedPackage <<= (mappings in Debian, target in Debian) map { (files, dir) =>
+    name in Debian := "sbt",
+    version in Debian <<= (version, sbtVersion) apply { (v, sv) =>       
+       sv + "-build-" + (v split "\\." map (_.toInt) dropWhile (_ == 0) map ("%02d" format _) mkString "")
+    },
+    target in Debian <<= (target, name in Debian, version in Debian) apply ((t,n, v) => t / (n +"-"+ v)),
+    genControlFile <<= (name in Debian, version in Debian, target in Debian) map {
+      (name, version, dir) =>
+        val cfile = dir / "DEBIAN" / "control"
+        IO.writer(cfile, ControlFileContent, java.nio.charset.Charset.defaultCharset, false) { o =>
+           val out = new java.io.PrintWriter(o)
+           out println ("Package: %s" format name)
+           out println ("Version: %s" format version)
+           out println ControlFileContent
+        }
+        cfile
+    },
+    makeDebianExplodedPackage <<= (mappings in Debian, genControlFile, target in Debian) map { (files, _, dir) =>
        for((file, target) <- files) {
           val tfile = dir / target
           if(file.isDirectory) IO.createDirectory(tfile)
@@ -26,10 +46,33 @@ object DebianPkg {
         }
         dir      
     },
-    packageBin in Debian <<= (makeDebianExplodedPackage, target in Debian) map { (pkgdir, tdir) =>
+    makeZippedPackageSource <<= (mappings in Debian in makeZippedPackageSource, target in Debian) map { (files, dir) =>
+        for((file, target) <- files) {
+          val tfile = dir / target
+          if(file.isDirectory) IO.createDirectory(tfile)
+          else {
+            val zipped = new File(tfile.getAbsolutePath + ".gz")
+            IO delete zipped
+            IO.copyFile(file,tfile)
+            Process(Seq("gzip", "--best", tfile.getAbsolutePath), Some(tfile.getParentFile)).!
+          }
+        }
+        dir      
+    },
+    packageBin in Debian <<= (makeDebianExplodedPackage, makeZippedPackageSource, target in Debian) map { (pkgdir, _, tdir) =>
        Process(Seq("dpkg-deb", "--build", pkgdir.getAbsolutePath), Some(tdir)).!
       tdir / "debian.deb"
     }
   )
-  
+
+
+  final val ControlFileContent = """Section: base
+Priority: optional
+Architecture: all
+Depends:   libcomerr2, libgnutls26, libldap-2.4-2, libcurl3, libsasl2-2, openjdk-6-jre-headless, libtinfo5, libtasn1-3, libgpg-error0, coreutils, libncurses5, libc6, bash (>= 2.05a-11)
+Maintainer: Josh Suereth <joshua.suereth@typesafe.com>
+Description: Simple Build Tool
+ This script provides a native way to run the Simple Build Tool,
+ a build tool for Scala software, also called SBT.
+"""
 }
