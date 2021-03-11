e96eaa1d5f1fa797c86d3c8816a9524e6f96b6fa
Mark Canlas <github@htmlism.com>
2021-03-11 11:04:44 -0500

    sbt 1.4.9

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -12,7 +12,7 @@ export sbt_10="0.10.1"
 export sbt_11="0.11.3"
 export sbt_12="0.12.4"
 export sbt_13="0.13.18"
-export sbt_1="1.4.8"
+export sbt_1="1.4.9"
 export sbt_release="$sbt_1"
 export sbt_dev="1.5.0-M2"
 
